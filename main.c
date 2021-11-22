c113f15613be275cd6e5bd784c348b3a613243a7
Ali-Johmson <94461028+Ali-Johmson@users.noreply.github.com>
2021-11-22 13:49:49 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+Project: Country Library management system
+Author: John K Githaiga
+Date: NOV, 2021
+Compiler: GNU, GCC
+Language: C99
+License: MIT
+*/
+# include <stdio.h>
+# include <stdio.h>
+Int main {}
+{
+Print f (“County Library\n”);
+Print f (“welcome mr. John. \n”);
+Print f (“what would you like to do?\n”);
+Print f (“1. Add new user\n”);
+Print f (“2. Add book. \n”);
+Print f (“selected action: ”);
+Return 0;
+}
